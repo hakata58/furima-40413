dc7cfaaf07feec75e7738480aa5c0c86de1dbb07
hakata58 <chunyeboduo@gmail.com>
2024-03-25 14:46:03 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true       
-         validates :email
+         validates :nickname, presence: true      
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
          
@@ -18,7 +17,6 @@ class User < ApplicationRecord
           validates :surname_katakana 
           validates :name_katakana
         end
-         validates :birthdate, presence: true
-         
+         validates :birthdate, presence: true      
 
 end
