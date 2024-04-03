3ea45fb433786bfcb851fc9a7cca72fa88942001
hakata58 <chunyeboduo@gmail.com>
2024-04-03 21:43:12 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new,:edit]
+ 
 
 
 
@@ -26,6 +27,20 @@ class ItemsController < ApplicationController
     @item=Item.find(params[:id])
     
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+    redirect_to item_path(params[:id])
+    else
+    render :edit, status: :unprocessable_entity
+    end
+  end
+
 
   private
 
